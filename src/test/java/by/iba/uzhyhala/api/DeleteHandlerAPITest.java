df57407b8666251a884e17860bf3136f890822e4
Uladzimir Zhyhala <zhigalo.v.y@gmail.com>
2018-05-10 18:30:17 +0300

    add test for DeleteHandlerAPI

---
+++ src/test/java/by/iba/uzhyhala/api/DeleteHandlerAPITest.java
@@ -0,0 +1,62 @@
+package by.iba.uzhyhala.api;
+
+import org.apache.struts.mock.MockHttpServletRequest;
+import org.apache.struts.mock.MockHttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore({"javax.xml.*", "org.xml.*", "org.w3c.*", "javax", "com.sun.org.apache.xerces.*"})
+public class DeleteHandlerAPITest {
+
+    private static final String QUERY_DELETE_LOT = "http://localhost:8080/del?" +
+            "uuid-lot=5c6b7e7c-aa1a-4e9a-94eb-31e9fe81f4c2&api_key=D2B331E0831A4C5683E17FDA0394723C";
+
+    @Mock
+    private SessionFactory sessionFactory;
+
+    @Mock
+    private Session session;
+
+    @Mock
+    private Transaction transaction;
+
+    @Mock
+    private Configuration configuration;
+
+    @Mock
+    private MockHttpServletRequest mockHttpServletRequest;
+
+    @Mock
+    private MockHttpServletResponse mockHttpServletResponse;
+
+    @Before
+    public void init() throws Exception {
+        initMocks(this);
+        when(sessionFactory.openSession()).thenReturn(session);
+        when(session.beginTransaction()).thenReturn(transaction);
+        sessionFactory = configuration.buildSessionFactory();
+
+        when(mockHttpServletRequest.getQueryString()).thenReturn(QUERY_DELETE_LOT);
+        when(mockHttpServletResponse.getWriter()).thenReturn(new PrintWriter(new StringWriter()));
+    }
+
+    @Test
+    public void test() {
+        new DeleteHandlerAPI().doGet(mockHttpServletRequest, mockHttpServletResponse);
+    }
+}
