46b5ffa7d08a6db637ee23a756f9e1fd08e5597b
Uladzimir Zhyhala <zhigalo.v.y@gmail.com>
2018-06-03 13:17:10 +0300

    fix for documentation

--- src/main/java/by/iba/uzhyhala/entity/BetEntity.java
+++ src/main/java/by/iba/uzhyhala/entity/BetEntity.java
@@ -6,39 +6,32 @@ import java.io.Serializable;
 @Entity
 @Table(name = "bet", schema = "public", catalog = "auction")
 public class BetEntity implements Serializable {
-
     private static final long serialVersionUID = -4773465060718156001L;
     private int id;
     private String uuid;
     private String bulk;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     @Basic
     @Column(name = "uuid", nullable = false, length = -1)
     public String getUuid() {
         return uuid;
     }
-
     public void setUuid(String uuid) {
         this.uuid = uuid;
     }
-
     @Basic
     @Column(name = "bulk", nullable = true, length = -1)
     public String getBulk() {
         return bulk;
     }
-
     public void setBulk(String bulk) {
         this.bulk = bulk;
     }
